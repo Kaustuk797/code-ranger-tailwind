2c8128740a800a118764104c227b8dcbcbe741b7
797_Kaustuk <87198200+Kaustuk797@users.noreply.github.com>
2022-05-21 01:47:56 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JS For Auto Typing Effect
+
+var typed = new Typed('#typedText', {
+    strings: ['Welcome!', 'स्वागत हे', 'willkommen', 'bienvenu', 'Yōkoso'],
+    typeSpeed: 120,
+    backSpeed: 120,
+    loop:true
+  });
+
+
